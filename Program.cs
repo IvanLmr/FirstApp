a40b801dd4ca29b184ce8ddb1f2c6ee709553d81
ivanfow <ivanfow@mail.ru>
2024-03-03 19:47:57 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Ivan";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
