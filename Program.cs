23ea9f422d9782b612bc154d4e7435e2824a7b34
ivanfow <ivanfow@mail.ru>
2024-03-04 22:08:23 +0500

    Анкета

--- Program.cs
+++ Program.cs
@@ -2,12 +2,21 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[])
         {
-            string MyName;
-            MyName = "Ivan";
-            Console.WriteLine(MyName);
-            Console.ReadKey();
+            string MyName = "Ivan";
+            Console.WriteLine("My name is " + MyName);
+            byte Age = 27;
+            Console.WriteLine("My Age " + Age);
+            bool Pet = true;
+            Console.WriteLine("Do you have a pet? " + Pet);
+            double MyShoe = 43.0;
+            Console.WriteLine("My shoe is size " + MyShoe);
+            
+            
+            
+
+
         }
     }
 }
