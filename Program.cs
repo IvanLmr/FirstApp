e863b7c86bf577671b817e6a23e3fb719d33e0bf
ivanfow <ivanfow@mail.ru>
2024-03-12 22:12:13 +0500

    4.1

--- Program.cs
+++ Program.cs
@@ -6,16 +6,32 @@ namespace _3
     {
         public static void Main(string[] args)
         {
-            Console.Write("Введите имя: ");
-            var name = Console.ReadLine();
-            
-            Console.Write("Введите возраст: ");
-            var age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ваше имя {0}, ваш возраст {1}", name, age);
-            Console.Write("Введите дату рождения: ");
-            var birthdate = Console.ReadLine();
-            Console.WriteLine("Ваша дата рождения {0}", birthdate);
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
             Console.ReadKey();
         }
          
